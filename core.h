ee54f30fc191e24c07197d5d117c99c0eff7973c
Harish <harishelumalai@hotmail.com>
2019-03-19 19:54:59 +0530

    Update core.h

--- core.h
+++ core.h
@@ -1,19 +1,22 @@
 #ifndef __CORE_H__
 #define __CORE_H__
 
-#define	SHMFILE		"shmfile"
-#define	SHMID		65
-#define SHMSIZE		1024
+#define SHMFILE         "shmfile"
+#define SHMID           65
+#define SHMSIZE         1024
 
-#define FAIL		-1
-#define SUCCEED		0
+#define FAIL            -1
+#define SUCCEED         0
+
+#define DEBUG(x)        fprintf(x)
 #define OUTFILE         stdout
 
 typedef struct
 {
-	char	svrname[40];
-	key_t	qkey;
-	int	mqid;
+        char    svrname[40];
+        key_t   qkey;
+        char    svcname[32];
+        int     mqid;
 }QUEUEROW_t;
 
 extern int tpinit();
