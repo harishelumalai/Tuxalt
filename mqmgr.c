ee911a9890ccf17542d793c5181d171dc2b803e1
Harish <harishelumalai@hotmail.com>
2019-03-19 19:55:42 +0530

    Update mqmgr.c

--- mqmgr.c
+++ mqmgr.c
@@ -7,66 +7,70 @@
 #include "core.h"
 #include "mqmgr.h"
 
-int create_mq(int key, int* mqid);      
+int create_mq(int key, int* mqid);
 int open_mq(int key, int *mqid);
-int write_to_mq(int mqid, MQMSG *msg); 
+int write_to_mq(int mqid, MQMSG *msg);
 int read_from_mq(int mqid, MQMSG *msg);
-int close_mq(int mqid);                
+int close_mq(int mqid);
 
 int create_mq(int key, int *mqid)
 {
-	if(*mqid = msgget(key, 0666|IPC_CREAT) <0)
-	{
-		fprintf(OUTFILE, "Unable to create queue for key[%d]\n", key);
-		return FAIL;
-	}
-	return *mqid;
+        if((*mqid = msgget(key, 0666|IPC_CREAT)) <0)
+        {
+                fprintf(OUTFILE, "Unable to create queue for key[%d]\n", key);
+                return FAIL;
+        }
+        fprintf(OUTFILE, "Mqid = [%d]\n", *mqid);
+        return *mqid;
 }
 
 int open_mq(int key, int *mqid)
 {
-	if(*mqid = msgget(key, 0666|IPC_CREAT|IPC_EXCL) <0)
-	{
-		fprintf(OUTFILE, "Unable to open queue for key[%d]\n", key);
-		return FAIL;
-	}
-	return *mqid;
+        if((*mqid = msgget(key, 0666|IPC_CREAT|IPC_EXCL)) <0)
+        {
+                fprintf(OUTFILE, "Unable to open queue for key[%d]\n", key);
+                return FAIL;
+        }
+        return *mqid;
 }
 
 int write_to_mq(int mqid, MQMSG *msg)
 {
-	int 	type;
-	if(0>mqid)
-	{
-		if(msgsnd(mqid, msg, sizeof(MQMSG), IPC_NOWAIT)<0)
-		{
-			fprintf(OUTFILE, "Unable to write to mq[%d]\n", mqid);
-		}
-	}
-	else
-		fprintf(OUTFILE, "WRITE: Invalid mqid [%d]\n", mqid);
+        int     type;
+        if(0>mqid)
+        {
+                if(msgsnd(mqid, msg, sizeof(MQMSG), IPC_NOWAIT)<0)
+                {
+                        fprintf(OUTFILE, "Unable to write to mq[%d]\n", mqid);
+                }
+        }
+        else
+                fprintf(OUTFILE, "WRITE: Invalid mqid [%d]\n", mqid);
 }
 
 int read_from_mq(int mqid, MQMSG *msg)
 {
-	int	type=GLOBAL_TYPE;
-	if(0>mqid)
-	{
-		if(msgrcv(mqid, msg, sizeof(MQMSG), type, 0)<0)
-		{
-			fprintf(OUTFILE, "Unable to read from mq[%d]\n", mqid);
-		}
-	}
-	else
-		fprintf(OUTFILE, "READ: Invalid mqid [%d]\n", mqid);
+        int     type=GLOBAL_TYPE;
+        if(0>mqid)
+        {
+                if(msgrcv(mqid, msg, sizeof(MQMSG), type, 0)<0)
+                {
+                        fprintf(OUTFILE, "Unable to read from mq[%d]\n", mqid);
+                }
+        }
+        else
+                fprintf(OUTFILE, "READ: Invalid mqid [%d]\n", mqid);
 }
 
 int close_mq(int mqid)
 {
-	if(0>mqid)
-	{
-		msgctl(mqid, IPC_RMID, NULL);
-	}
-	else
-		fprintf(OUTFILE, "CLOSE: Invalid mqid [%d]\n", mqid);
+        int ret=FAIL;
+        if(mqid>0)
+        {
+                ret=msgctl(mqid, IPC_RMID, NULL);
+                return ret;
+        }
+        else
+                fprintf(OUTFILE, "CLOSE: Invalid mqid [%d]\n", mqid);
+        return FAIL;
 }
